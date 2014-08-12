042db49c253a123f75dc3463d0ccf721b66ca060
Efrén Reyes <efren.reyes@globant.com>
2014-08-12 10:19:17 -0500

    Initial Commit

---
+++ TroyStory4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TroyStory4
+//
+//  Created by Efrén Reyes Torres on 8/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
